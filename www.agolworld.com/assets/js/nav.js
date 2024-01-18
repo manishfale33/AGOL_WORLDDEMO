04b7786ee10a4d40e0c250114bd2a2a12b9caf42
Manish Fale <manishfale33@gmail.com>
2024-01-19 00:17:15 +0530

    19/01/2024/00.17

--- www.agolworld.com/assets/js/nav.js
+++ www.agolworld.com/assets/js/nav.js
@@ -1,6 +1,18 @@
 document.addEventListener("DOMContentLoaded", function () {
-    window.addEventListener("scroll", function () {
-        var header = document.querySelector('.navbar-area');
-        header.classList.toggle('sticky', window.scrollY > 0);
+    let prevScrollPos = window.pageYOffset;
+    const navbar = document.querySelector('.navbar-area');
+
+    window.addEventListener('scroll', function () {
+        const currentScrollPos = window.pageYOffset;
+
+        if (prevScrollPos > currentScrollPos) {
+            // Scrolling up
+            navbar.classList.remove('navbar-hidden');
+        } else {
+            // Scrolling down
+            navbar.classList.add('navbar-hidden');
+        }
+
+        prevScrollPos = currentScrollPos;
     });
 });
