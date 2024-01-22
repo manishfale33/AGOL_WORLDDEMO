d025b5962c1aca7b67d66ce0314b944622eb6869
Manish Fale <manishfale33@gmail.com>
2024-01-22 22:40:57 +0530

    22/01/24/22.40

--- www.agolworld.com/assets/js/nav.js
+++ www.agolworld.com/assets/js/nav.js
@@ -8,9 +8,11 @@ document.addEventListener("DOMContentLoaded", function () {
         if (prevScrollPos > currentScrollPos) {
             // Scrolling up
             navbar.classList.remove('navbar-hidden');
+            navbar.style.backgroundColor = 'rgba(255, 255, 255, 0.8)'; // Adjust alpha value as needed
         } else {
             // Scrolling down
             navbar.classList.add('navbar-hidden');
+            navbar.style.backgroundColor = 'rgba(255, 255, 255, 1)'; // Set to fully opaque or adjust as needed
         }
 
         prevScrollPos = currentScrollPos;
