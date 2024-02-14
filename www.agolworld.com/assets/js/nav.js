7ff553c60ee3e9c9b198ec0ff8afaff9e257e6fb
Manish Fale <manishfale33@gmail.com>
2024-02-14 12:04:08 +0530

    12.03.14.02.24

--- www.agolworld.com/assets/js/nav.js
+++ www.agolworld.com/assets/js/nav.js
@@ -1,20 +1,9 @@
-document.addEventListener("DOMContentLoaded", function () {
-    let prevScrollPos = window.pageYOffset;
-    const navbar = document.querySelector('.navbar-area');
-
-    window.addEventListener('scroll', function () {
-        const currentScrollPos = window.pageYOffset;
-
-        if (prevScrollPos > currentScrollPos) {
-            // Scrolling up
-            navbar.classList.remove('navbar-hidden');
-            navbar.style.backgroundColor = 'rgba(255, 255, 255, 0.8)'; // Adjust alpha value as needed
-        } else {
-            // Scrolling down
-            navbar.classList.add('navbar-hidden');
-            navbar.style.backgroundColor = 'rgba(255, 255, 255, 1)'; // Set to fully opaque or adjust as needed
-        }
-
-        prevScrollPos = currentScrollPos;
-    });
+window.addEventListener('scroll', function() {
+    var navbar = document.querySelector('.navbar-area');
+    var scrollTop = window.pageYOffset || document.documentElement.scrollTop;
+    if (scrollTop > 0) {
+        navbar.classList.add('is-sticky');
+    } else {
+        navbar.classList.remove('is-sticky');
+    }
 });
---
+++ www.agolworld.com/assets/js/typed.js
@@ -0,0 +1,7 @@
+var typed = new Typed('#typed-text', {
+    strings: ['ACCELERATE YOUR SHIPMENT WITH<br/>AGOL Worldwide OBC!'],
+    typeSpeed: 50, // Adjust typing speed
+    startDelay: 1000, // Delay before typing starts
+    showCursor: false, // Hide cursor
+    // Optionally, you can add more configuration options here
+  });
